5f34c4eafc14d5c638b44c8f66363a2843fb02e3
err0rn1ck <dim.min.6630@mail.ru>
2025-04-07 21:01:43 +0300

    Дополнение конспекта по googletests

--- libs/googletests/tests.cpp
+++ libs/googletests/tests.cpp
@@ -1,14 +1,19 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include <sstream>
 #include "main.h"
 
-TEST(TestSuitName, TestName) {
+TEST(ExpectTests, ExpectTest) {
     int a = 10;
 
     EXPECT_TRUE(func(a) == 2 * a);
     EXPECT_FALSE(func(a) != 2 * a);
     EXPECT_EQ(func(a), 2*a);
     EXPECT_THROW(func(std::numeric_limits<int>::max()/2 + 1), std::runtime_error);
+}
+
+TEST(StreamTests, StreamTest) {
+    int a = 10;
 
     std::stringstream buffer;
     std::streambuf* old_cout = std::cout.rdbuf(buffer.rdbuf());
@@ -17,4 +22,11 @@ TEST(TestSuitName, TestName) {
     std::cout.rdbuf(old_cout);
 
     EXPECT_EQ(buffer.str(), "result: 10");
+}
+
+TEST(ContainerTests, ContainerTest) {
+    std::vector<int> container{0, 1, 2, 3, 4, 5};
+
+    ASSERT_THAT(container, ::testing::ElementsAre(0, 1, 2, 3, 4, 5));
+    ASSERT_THAT(container, ::testing::UnorderedElementsAre(4, 5, 0, 1, 2, 3));
 }
